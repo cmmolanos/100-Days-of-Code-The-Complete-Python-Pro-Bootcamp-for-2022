ef5720a895eaa6c0154b6799d741849ae3bf3a66
Carlos Molano <51679038+cmmolanos1@users.noreply.github.com>
2022-08-03 15:21:50 -0500

    Push Day 53.

--- Day_48_Selenium/interaction.py
+++ Day_48_Selenium/interaction.py
@@ -1,9 +1,10 @@
+import os
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import  Keys
 
 
-driver = webdriver.Chrome(executable_path="driver path")
+driver = webdriver.Chrome(executable_path=os.environ.)
 
 # driver.get("https://en.wikipedia.org/wiki/Main_Page")
 #
